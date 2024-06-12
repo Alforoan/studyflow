d11cb48c8cd90788fccb8f60aca15506cddcb8c3
Jason Morgan <morgannj71@gmail.com>
2024-06-11 21:46:44 -0700

    connected links with pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Account from "./pages/Account";
+
 function App() {
-	return (
-		<Router>
-			<Navbar />
-			<Home />
-		</Router>
-	);
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/logout" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
