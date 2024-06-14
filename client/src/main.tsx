3f597acf9f4b84afdc8f2f2c13ee703415224030
Jason Morgan <morgannj71@gmail.com>
2024-06-13 17:18:41 -0700

    authhandler

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
+import AuthHandler from './components/AuthHandler.tsx';
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       clientId={clientId}
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
+      <AuthHandler />
       <App />
     </Auth0Provider>
   </React.StrictMode>,
