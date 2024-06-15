c654d8b30134b781f86d29892ae801859d4f5571
Jason Morgan <morgannj71@gmail.com>
2024-06-15 15:57:53 -0700

    added simple responsive view landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Account from "./pages/Account";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/account" element={<Account />} />
-        <Route path="/logout" element={<Home />} />
+        {/* <Route path="/logout" element={<Home />} /> */}
       </Routes>
     </Router>
   );
