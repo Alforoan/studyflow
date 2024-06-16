0791bec81bec8ad5d2e5d4ee18c4dc30f703e4fd
Jason Morgan <morgannj71@gmail.com>
2024-06-15 18:30:32 -0700

    fixed page render after log in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Account from "./pages/Account";
 import Landing from "./pages/Landing";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Landing />} />
+        {!isAuthenticated ? (
+          <Route path="/" element={<Landing />} />
+        ) : (
+          <Route path="/" element={<Navigate to="/home" />} />
+        )}
         <Route path="/home" element={<Home />} />
         <Route path="/account" element={<Account />} />
         {/* <Route path="/logout" element={<Home />} /> */}
@@ -19,4 +26,3 @@ function App() {
 }
 
 export default App;
-
