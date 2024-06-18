55663f7f46731564db3cb2e597c5010274709e05
devkthinesrsc <khines@paqinc.com>
2024-06-18 14:30:53 -0700

    adding proxy and other things with postman etc etc

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/flaskdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5000/flaskdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -64,6 +64,8 @@ def create_board():
         data = request.json
         email = data.get('email')
         name = data.get('name')
+        print('email', email)
+        print('name', name)
         user = User.query.filter_by(email=email).first()
         name_exists = Board.query.filter_by(name=name).first()
         user_id = user.id
@@ -163,8 +165,8 @@ def delete_card(card_id):
             return jsonify({'error': 'Card not found'}), 404
     else:
         return jsonify({'error': 'Only DELETE requests are allowed for this endpoint'}), 405
-    
 
+  
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
