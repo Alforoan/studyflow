bea2aa620f252f7696a9903bbe6afa12bc41d648
Jay <jdragon2050@gmail.com>
2024-06-24 17:47:07 -0700

    fix spacing and quotes

--- client/src/components/CreateCardComponent.tsx
+++ client/src/components/CreateCardComponent.tsx
@@ -72,83 +72,83 @@ const CreateCardComponent: React.FC<CreateCardComponentProps> = ({
 	};
 
 	return (
-    <div className='p-4 w-1/2 mx-auto bg-secondaryElements shadow-md rounded-lg'>
-      <h2 className='text-lg font-bold mb-4'>Create New Card</h2>
-      <form onSubmit={handleSubmit}>
-        <label className='block mb-2'>
-          Card Name:
-          <input
-            type='text'
-            value={cardName}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setCardName(e.target.value)
-            }
-            className='rounded px-2 py-1 mb-2 w-full'
-          />
-        </label>
-        <label className='block mb-2'>
-          Notes:
-          <textarea
-            value={notes}
-            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-              setNotes(e.target.value)
-            }
-            className='rounded px-2 py-1 mb-2 w-full'
-          />
-        </label>
-        <label className='block mb-2'>
-          Time Estimate (minutes):
-          <input
-            type='number'
-            value={timeEstimate}
-            onChange={handleTimeEstimateChange}
-            className='rounded px-2 py-1 mb-2 w-full'
-          />
-        </label>
-        <div className='mb-4'>
-          <h3 className='font-bold mb-2'>Checklist</h3>
-          <ul>
-            {checklistItems.map((item, index) => (
-              <li key={index} className='mb-1 flex items-center'>
-                <input
-                  type='checkbox'
-                  checked={item.checked}
-                  onChange={() => {}} // Placeholder for change handler
-                />
-                {item.value}
-              </li>
-            ))}
-          </ul>
-          <div className='flex'>
-            <input
-              type='text'
-              value={newChecklistItem}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setNewChecklistItem(e.target.value)
-              }
-              className='rounded px-2 py-1 mr-2 flex-grow'
-              placeholder='Add checklist item'
-            />
-            <button
-              onClick={handleAddChecklistItem}
-              className='ml-2 bg-flair font-primary text-secondaryElements px-4 py-2 rounded hover:text-white'
-            >
-              Add
-            </button>
-          </div>
-        </div>
-        <button
-          type='submit'
-          className=' bg-flair font-primary text-secondaryElements px-4 py-2 rounded hover:text-white'
-        >
-          Create Card
-        </button>
-        <button onClick={() => handleResetSelectedCard()} className=' bg-flair font-primary text-secondaryElements px-4 py-2 rounded hover:text-white ml-4'>
+		<div className="p-4 w-1/2 mx-auto bg-secondaryElements shadow-md rounded-lg">
+			<h2 className="text-lg font-bold mb-4">Create New Card</h2>
+			<form onSubmit={handleSubmit}>
+				<label className="block mb-2">
+					Card Name:
+					<input
+						type="text"
+						value={cardName}
+						onChange={(e: ChangeEvent<HTMLInputElement>) =>
+							setCardName(e.target.value)
+						}
+						className="rounded px-2 py-1 mb-2 w-full"
+					/>
+				</label>
+				<label className="block mb-2">
+					Notes:
+					<textarea
+						value={notes}
+						onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+							setNotes(e.target.value)
+						}
+						className="rounded px-2 py-1 mb-2 w-full"
+					/>
+				</label>
+				<label className="block mb-2">
+					Time Estimate (minutes):
+					<input
+						type="number"
+						value={timeEstimate}
+						onChange={handleTimeEstimateChange}
+						className="rounded px-2 py-1 mb-2 w-full"
+					/>
+				</label>
+				<div className="mb-4">
+					<h3 className="font-bold mb-2">Checklist</h3>
+					<ul>
+						{checklistItems.map((item, index) => (
+							<li key={index} className="mb-1 flex items-center">
+								<input
+									type="checkbox"
+									checked={item.checked}
+									onChange={() => {}} // Placeholder for change handler
+								/>
+								{item.value}
+							</li>
+						))}
+					</ul>
+					<div className="flex">
+						<input
+							type="text"
+							value={newChecklistItem}
+							onChange={(e: ChangeEvent<HTMLInputElement>) =>
+								setNewChecklistItem(e.target.value)
+							}
+							className="rounded px-2 py-1 mr-2 flex-grow"
+							placeholder="Add checklist item"
+						/>
+						<button
+							onClick={handleAddChecklistItem}
+							className="ml-2 bg-flair font-primary text-secondaryElements px-4 py-2 rounded hover:text-white"
+						>
+							Add
+						</button>
+					</div>
+				</div>
+				<button
+					type="submit"
+					className=" bg-flair font-primary text-secondaryElements px-4 py-2 rounded hover:text-white"
+				>
+					Create Card
+				</button>
+				<button onClick={() => handleResetSelectedCard()} className="bg-flair font-primary text-secondaryElements px-4 py-2 rounded hover:text-white ml-4">
           Cancel
         </button>
-      </form>
-    </div>
-  );
+			</form>
+		</div>
+	);
 };
 
 export default CreateCardComponent;
