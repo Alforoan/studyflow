9621e78ce3ca22ab05541ae27bdfd3478a52ed6a
Jason Morgan <morgannj71@gmail.com>
2024-06-26 11:40:29 -0700

    refactored progress bar and added tests

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,1 +1,20 @@
 import "@testing-library/jest-dom";
+
+interface ImportMetaEnv {
+  VITE_AUTH0_DOMAIN: string;
+  VITE_AUTH0_CLIENT_ID: string;
+}
+
+const importMetaEnv: ImportMetaEnv = {
+  VITE_AUTH0_DOMAIN: "mock-domain",
+  VITE_AUTH0_CLIENT_ID: "mock-client-id",
+};
+
+// Assert global as unknown first, then cast to a specific type
+(global as unknown as { importMetaEnv: ImportMetaEnv }).importMetaEnv = importMetaEnv;
+
+Object.defineProperty(global, 'import.meta', {
+  value: {
+    env: importMetaEnv,
+  },
+});
