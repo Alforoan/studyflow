70b718ccfcc46dfbd333cdfbb98f55f6fd8c46ee
Jay <jdragon2050@gmail.com>
2024-06-28 12:11:54 -0700

    add allowed origin for cross origin connection

--- server/app.py
+++ server/app.py
@@ -13,7 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = f'{os.getenv("SECRET")}'
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+allowed_origins = ["http://localhost:5173", "https://https://studyflow.onrender.com"]
+cors = CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
