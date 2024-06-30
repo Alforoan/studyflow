80e3da231701db152fb62f04f828db084e01c83d
JeremySpence272 <jeremy.spence272@gmail.com>
2024-06-29 20:00:07 -0400

    update test

--- client/src/tests/EditBoardName.spec.tsx
+++ client/src/tests/EditBoardName.spec.tsx
@@ -14,6 +14,7 @@ jest.mock("axios");
 //   cards: [],
 // };
 
+console.log("huh");
 describe("EditBoardName Component", () => {
   // test("renders the edit button", () => {
   //   render(<EditBoardName board={board} />);
