f4c8cc4e5d2d1adfe3c25ad83693d007ffa33a17
JeremySpence272 <jeremy.spence272@gmail.com>
2024-06-30 14:55:30 -0400

    updated checkboxes & made drag and drop smoother plus fixed dnd bug

--- client/src/context/BoardContext.tsx
+++ client/src/context/BoardContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, ReactNode } from "react";
-import { Board, Card } from "../types";
+import { Board, Card, Columns } from "../types";
 import usePostNewBoard from "../hooks/usePostNewBoard";
 
 import usePostNewCard from "../hooks/usePostNewCard";
